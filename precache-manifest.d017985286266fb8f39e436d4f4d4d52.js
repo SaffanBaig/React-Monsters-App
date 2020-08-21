74945a5e56accb77972b0941515ba74b8ea9ceaa
Saffan Baig <mirzasaffan492@gmail.com>
2020-08-21 12:06:18 +0500

    Updates

---
+++ precache-manifest.d017985286266fb8f39e436d4f4d4d52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee28e3b27a7fd83c364fbd663d4e3213",
+    "url": "/React-Monsters-App/index.html"
+  },
+  {
+    "revision": "6b0acbd034c14fee18b9",
+    "url": "/React-Monsters-App/static/css/main.e81b4439.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/React-Monsters-App/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Monsters-App/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b0acbd034c14fee18b9",
+    "url": "/React-Monsters-App/static/js/main.cc2b964c.chunk.js"
+  },
+  {
+    "revision": "809b30b77ad549de5e68",
+    "url": "/React-Monsters-App/static/js/runtime-main.1038675c.js"
+  }
+]);
